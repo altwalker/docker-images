87f2be2e3f5048d62d146c640909fa1f8dc9e8c1
Robert Dezmerean <robert.dezmerean@altom.com>
2020-03-05 12:34:12 +0200

    Add tox image for tests

--- tests/tox/install-python.sh
+++ tests/tox/install-python.sh
@@ -4,8 +4,8 @@ PYTHON_BASE_URL="https://www.python.org/ftp/python/"
 echo "Installing CPython: $PYTHON_VERSION..."
 
 url="$PYTHON_BASE_URL$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz"
-wget $url
-tar xzvf "Python-$PYTHON_VERSION.tgz"
+wget --no-verbose $url
+tar xzvf "Python-$PYTHON_VERSION.tgz" > /dev/null
 cd "Python-$PYTHON_VERSION"
 
 ./configure
